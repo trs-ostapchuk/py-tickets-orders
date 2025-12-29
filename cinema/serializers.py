568f17dc025b3396de1ee9b6a111cb14ca176372
Taras <tarasostapchuk27@gmail.com>
2025-12-29 10:35:34 +0100

    Add TicketSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Ticket
+)
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -78,3 +85,10 @@ class MovieSessionDetailSerializer(MovieSessionSerializer):
     class Meta:
         model = MovieSession
         fields = ("id", "show_time", "movie", "cinema_hall")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "movie_session")
