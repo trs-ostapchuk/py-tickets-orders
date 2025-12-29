78311358af4611ccda33f1723accaac2b2f1737e
Taras <tarasostapchuk27@gmail.com>
2025-12-29 12:52:51 +0100

    Add method for creating order with related tickets

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from cinema.models import (
@@ -97,8 +98,20 @@ class TicketSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    tickets = TicketSerializer(many=True)
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
 
     class Meta:
         model = Order
         fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        """
+        Create an order and its related tickets
+        within a single atomic transaction
+        """
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
