0a0a9496794f709f752f96bb888acb5c8cb59254
Taras <tarasostapchuk27@gmail.com>
2026-01-08 19:05:26 +0100

    Modify pagination in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,6 +137,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-   "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-   "PAGE_SIZE": 2
+   "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+   "PAGE_SIZE": 10
 }
